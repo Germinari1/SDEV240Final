4391dfeacfa46094f4aa8da45726a3ab1a8c3f47
Michael Maddin <michaelmaddinii@gmail.com>
2023-12-08 12:37:22 -0500

    Reupload from dev2

--- Material_List_Estimator/Material List Estimator/Form1.cs
+++ Material_List_Estimator/Material List Estimator/Form1.cs
@@ -12,6 +12,7 @@ namespace Material_List_Estimator
 {
     public partial class Form1 : Form
     {
+<<<<<<< Updated upstream
         public Form1()
         {
             InitializeComponent();
@@ -20,6 +21,27 @@ namespace Material_List_Estimator
         private void label1_Click(object sender, EventArgs e)
         {
 
+=======
+        //declaring item object
+        private Item _item;
+        //list of items
+        BindingList<Item> items;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            _item = new Item();
+
+            // Set up list and datagridview
+            items = new BindingList<Item>();
+            //bind data source
+            gridItems.DataSource = items;
+
+            //[TEST]
+            Item item = new Item("wood", 5000, 3, "construction wood");
+            items.Add(item);
+>>>>>>> Stashed changes
         }
     }
 }
