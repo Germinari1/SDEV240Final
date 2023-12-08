bc1fe17bffec0162c3f925dcc903f60f0f816609
Lucas Germinari <127156160+Germinari1@users.noreply.github.com>
2023-12-08 15:02:58 -0500

    Add files via upload

---
+++ SDEV240Final-main/SDEV240Final-main/Material_List_Estimator/Material List Estimator/Form1.Designer.cs
@@ -0,0 +1,338 @@
+﻿namespace Material_List_Estimator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtHomeName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.gridItems = new System.Windows.Forms.DataGridView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtTotalCost = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtItem = new System.Windows.Forms.TextBox();
+            this.txtMaterial = new System.Windows.Forms.TextBox();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.labelPrice = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtQuantity = new System.Windows.Forms.NumericUpDown();
+            this.txtPriceCents = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.gridItems)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtQuantity)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Home Name";
+            // 
+            // txtHomeName
+            // 
+            this.txtHomeName.Location = new System.Drawing.Point(141, 62);
+            this.txtHomeName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtHomeName.Name = "txtHomeName";
+            this.txtHomeName.Size = new System.Drawing.Size(128, 22);
+            this.txtHomeName.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(71, 156);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 16);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Material";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(292, 52);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 34);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Save File";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // gridItems
+            // 
+            this.gridItems.AllowUserToDeleteRows = false;
+            this.gridItems.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gridItems.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridItems.Location = new System.Drawing.Point(292, 91);
+            this.gridItems.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.gridItems.Name = "gridItems";
+            this.gridItems.ReadOnly = true;
+            this.gridItems.RowHeadersWidth = 51;
+            this.gridItems.RowTemplate.Height = 24;
+            this.gridItems.Size = new System.Drawing.Size(496, 318);
+            this.gridItems.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(68, 255);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Quantity";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(292, 416);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(76, 33);
+            this.btnClear.TabIndex = 8;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(373, 416);
+            this.btnCalculate.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(129, 33);
+            this.btnCalculate.TabIndex = 9;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(537, 420);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(86, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Total Cost";
+            // 
+            // txtTotalCost
+            // 
+            this.txtTotalCost.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.txtTotalCost.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTotalCost.Location = new System.Drawing.Point(653, 417);
+            this.txtTotalCost.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtTotalCost.Name = "txtTotalCost";
+            this.txtTotalCost.ReadOnly = true;
+            this.txtTotalCost.Size = new System.Drawing.Size(135, 26);
+            this.txtTotalCost.TabIndex = 11;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(141, 349);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(128, 30);
+            this.btnAdd.TabIndex = 12;
+            this.btnAdd.Text = "Add to List";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(99, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 16);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Item";
+            // 
+            // txtItem
+            // 
+            this.txtItem.Location = new System.Drawing.Point(141, 110);
+            this.txtItem.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtItem.Name = "txtItem";
+            this.txtItem.Size = new System.Drawing.Size(128, 22);
+            this.txtItem.TabIndex = 13;
+            // 
+            // txtMaterial
+            // 
+            this.txtMaterial.Location = new System.Drawing.Point(141, 156);
+            this.txtMaterial.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtMaterial.Name = "txtMaterial";
+            this.txtMaterial.Size = new System.Drawing.Size(128, 22);
+            this.txtMaterial.TabIndex = 14;
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(141, 203);
+            this.txtDescription.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(128, 22);
+            this.txtDescription.TabIndex = 16;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(49, 207);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(75, 16);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Description";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(141, 298);
+            this.txtPrice.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(87, 22);
+            this.txtPrice.TabIndex = 18;
+            this.txtPrice.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.PriceKeyPress);
+            // 
+            // labelPrice
+            // 
+            this.labelPrice.AutoSize = true;
+            this.labelPrice.Location = new System.Drawing.Point(68, 302);
+            this.labelPrice.Name = "labelPrice";
+            this.labelPrice.Size = new System.Drawing.Size(48, 16);
+            this.labelPrice.TabIndex = 17;
+            this.labelPrice.Text = "Price $";
+            this.labelPrice.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 393);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(268, 16);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "*Item, Price, and Quantity are required fields ";
+            // 
+            // txtQuantity
+            // 
+            this.txtQuantity.Location = new System.Drawing.Point(141, 255);
+            this.txtQuantity.Name = "txtQuantity";
+            this.txtQuantity.Size = new System.Drawing.Size(128, 22);
+            this.txtQuantity.TabIndex = 20;
+            // 
+            // txtPriceCents
+            // 
+            this.txtPriceCents.Location = new System.Drawing.Point(239, 298);
+            this.txtPriceCents.Name = "txtPriceCents";
+            this.txtPriceCents.Size = new System.Drawing.Size(30, 22);
+            this.txtPriceCents.TabIndex = 21;
+            this.txtPriceCents.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.PriceKeyPress);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(229, 295);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(17, 25);
+            this.label8.TabIndex = 22;
+            this.label8.Text = ".";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(911, 586);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtPriceCents);
+            this.Controls.Add(this.txtQuantity);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.labelPrice);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtMaterial);
+            this.Controls.Add(this.txtItem);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtTotalCost);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.gridItems);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtHomeName);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MinimumSize = new System.Drawing.Size(929, 633);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.gridItems)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtQuantity)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtHomeName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.DataGridView gridItems;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtTotalCost;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtItem;
+        private System.Windows.Forms.TextBox txtMaterial;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.Label labelPrice;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.NumericUpDown txtQuantity;
+        private System.Windows.Forms.TextBox txtPriceCents;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ SDEV240Final-main/SDEV240Final-main/Material_List_Estimator/Material List Estimator/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Material_List_Estimator
+{
+    public partial class Form1 : Form
+    {
+        //declaring item object
+        private Item _item;
+        //list of items
+        BindingList<Item> items;
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+            _item = new Item();
+
+            // Set up list and datagridview
+            items = new BindingList<Item>();
+            //bind data source
+            gridItems.DataSource = items;
+
+            //[TEST]
+            Item item = new Item("wood", 5000, 3, "construction wood");
+            items.Add(item);
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            /////adds a new item to the table
+
+            //check if required fields have user-generate data. If yes, add the item
+            if (txtItem.TextLength!=0 && txtPrice.TextLength!=0)
+            {
+                //instanciate new Item
+                Item newItem = new Item();
+                //populate fields of new item
+                newItem.ItemName = txtItem.Text;
+                newItem.Qnt = Convert.ToInt32(txtQuantity.Text);
+                newItem.Decription = txtDescription.Text;
+                newItem.Material = txtMaterial.Text;
+                newItem.Price = Convert.ToDouble(txtPrice.Text);
+                //add new item to list 
+                items.Add(newItem);
+            }
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            /////clears the data source of the datagrid (also clearing the table as a consequence) and input fields
+            
+            //clear data source (list)
+            items.Clear();
+
+            //clear input fields
+            txtDescription.Clear();
+            txtPrice.Clear();
+            txtMaterial.Clear();
+            txtPriceCents.Clear();
+            txtItem.Clear();
+            txtHomeName.Clear();
+            txtQuantity.Value = txtQuantity.Minimum;
+            
+        }
+
+        private void PriceKeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Allow only numbers
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
